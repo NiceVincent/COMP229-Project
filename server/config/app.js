d01e692eccc9a3328e68d190de2bc782fdd6910a
v <>
2023-04-10 03:37:48 -0400

    build: change api url to fit the f2e setting

--- server/config/app.js
+++ server/config/app.js
@@ -67,7 +67,7 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/videos", videosRouter);
+app.use("/api/videos", videosRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
